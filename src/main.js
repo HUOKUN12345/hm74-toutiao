f77ba8417648dcfda9f36013dfac4aadc6531bfc
chenxin <1787428500@qq.com>
2019-07-09 16:30:58 +0800

    dsd

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 职责1：导入项目需要的依赖资源（包css 。。。）
+// 职责2：初始化根实例 new Vue()选项render 作用是把某一个app组件渲染在你指定的容器$mount('#app')内
 new Vue({
   render: h => h(App)
 }).$mount('#app')
